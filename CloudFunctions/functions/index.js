c9dc96cacd454f3c243359ea6cca1a745e187e5a
Alex Santos <asobrasil@yahoo.com.br>
2019-03-22 17:21:12 -0300

    Notificação modificada

--- CloudFunctions/functions/index.js
+++ CloudFunctions/functions/index.js
@@ -6,26 +6,24 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 exports.sendFavoriteNotifications = functions.firestore.document("/semanas/{semana}/atividades/{atividade}").onUpdate(event =>{
-  const atividade = event.after.ref.path.split("/")[3];
-  
-  const notificationContent = {
-    notification: {
-      title: "Isso ai",
-      body: "Agora foi de novo",
-    }
-  };
-  
-  return admin.firestore().collection("users").get().then(users => {
+    const splitedPath = event.after.ref.path.split("/");
 
-    users.forEach(user => {
-      const favorites = admin.firestore().collection("users").doc(user.data().uid).collection("favorites");
-      favorites.where("id", "=", atividade).get().then(favoritouEvento => {
-        if(!favoritouEvento.empty){
-          admin.messaging().sendToDevice(user.data().token, notificationContent);
-        }
-      });
+    return admin.firestore().collection("semanas").doc(splitedPath[1]).get().then(semanaModificada => {
+        const notificationContent = {
+            notification: {
+                title: semanaModificada.data().nome,
+                body: event.after.data().nome + " sofreu uma alteração, venha ver!",
+            }
+        };
+
+        admin.firestore().collection("users").get().then(users => {
+            users.forEach(user => {
+                const favorites = admin.firestore().collection("users").doc(user.data().uid).collection("favorites");
+                favorites.where("id", "=", splitedPath[3]).get().then(favoritouEvento => {
+                    if(!favoritouEvento.empty) admin.messaging().sendToDevice(user.data().token, notificationContent);
+                });
+            });
+        });
+        
     });
-
-    return 0;
-  });
 });
